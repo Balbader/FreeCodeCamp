b9532a29f81d15f1704a495a6f7cd9db441da03f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-22 20:51:24 +0200

    update

--- JS Algorithms and Data Structures Certification/largest_num_in_arr.js
+++ JS Algorithms and Data Structures Certification/largest_num_in_arr.js
@@ -12,5 +12,44 @@ function largestOfFour(arr) {
     return (newArr);
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
+
+// Prodedural Approach
+function largestOfFour(arr) {
+    let results = [];
+    for (let i = 0; i < arr.length; i++) {
+        let largestNumber = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > largestNumber) {
+                largestNumber = arr[i][j];
+            }
+        }
+        results[i] = largestNumber;
+    }
+
+    return results;
+}
+
+
+// Declarative approach
+function largestOfFour(arr) {
+    return arr.map(function(group) {
+        return group.reduce(function(prev, current) {
+            return current > prev ? current : prev;
+        });
+    });
+}
+
+
+// Declarative approach #2
+function largestOfFour(arr) {
+    return arr.map(Function.apply.bind(Math.max, null));
+}
+
+
+// Recursive Approach
+function largestOfFour(arr, finalArr = []) {
+    return !arr.length
+        ? finalArr
+        : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
+}
