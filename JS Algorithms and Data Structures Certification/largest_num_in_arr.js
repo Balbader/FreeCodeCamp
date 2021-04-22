ac550394ba3c75b15435666485b4f1756b792be0
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-22 20:46:55 +0200

    update

---
+++ JS Algorithms and Data Structures Certification/largest_num_in_arr.js
@@ -0,0 +1,16 @@
+function largestOfFour(arr) {
+    let newArr = [];
+    let largestNumber = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            largestNumber = Math.max(...arr[i]);
+            if (j == arr[i].length)
+                largestNumber == 0;
+        }
+        newArr.push(largestNumber);
+    }
+    return (newArr);
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
