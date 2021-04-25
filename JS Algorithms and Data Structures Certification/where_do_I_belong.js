b035c87c7d4546d54161bf8e9daba277e442f20b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-25 17:57:35 +0200

    update

--- JS Algorithms and Data Structures Certification/where_do_I_belong.js
+++ JS Algorithms and Data Structures Certification/where_do_I_belong.js
@@ -9,7 +9,15 @@ and greater than 5 (index 1).
 */
 
 function getIndexToIns(arr, num) {
+    arr.sort((a, b) => {
+        return a - b;
+    });
+    console.log(arr, num);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) {
+            return (i);
+        }
+    }
+    return arr.length;
 
 }
-
-getIndexToIns ([40, 60], 50);
