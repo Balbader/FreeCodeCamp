7cbba00d26c45bc5532f2683ab29103c0abbb65b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-25 18:48:39 +0200

    update

--- JS Algorithms and Data Structures Certification/mutation.js
+++ JS Algorithms and Data Structures Certification/mutation.js
@@ -8,3 +8,14 @@ does not contain a y.
 Lastly, ["Alien", "line"], should return true because all of the letters
 in line are present in Alien.
 */
+function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+        if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+}
+
+console.log(mutation(["hello", "hey"])); // => false
+console.log(mutation(["hello", "hell"])); // => true
