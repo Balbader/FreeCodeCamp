5412970c328c0eaa07e0b04b911b3497184ebbd1
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-28 17:19:20 +0200

    file deletion

--- JS Algorithms and Data Structures Certification/strReverse.js
+++
@@ -1,8 +0,0 @@
-const reverseString = (str) => {
-    let splitStr = str.split('');
-    let reverseStr = splitStr.reverse();
-    let joinStr = reverseStr.join('');
-    return joinStr;
-}
-
-console.log(reverseString('hello'));
