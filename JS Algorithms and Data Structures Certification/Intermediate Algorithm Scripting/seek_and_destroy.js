e403b39165c93b281a70cdbe6d2eb544dc1b9b83
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-29 17:27:54 +0200

    update

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/seek_and_destroy.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/seek_and_destroy.js
@@ -5,8 +5,9 @@ the initial array that are of the same value as these arguments.
 Note: You have to use the arguments object.
 */
 
-function destroyer(arr) {
-    return arr;
+function destroyer(arr, ...args) {
+    return arr.filter(elem => !args.includes(elem));
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
