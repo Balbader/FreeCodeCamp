5dad6271d9201bae97a2127c884130d53f5d2e68
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-30 18:14:12 +0200

    update

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/spinal_tap_case.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/spinal_tap_case.js
@@ -1,21 +1,13 @@
 /*Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.*/
 
 function spinalCase(str) {
-    let newStr = '';
-    newStr = str.replace(/[^a-zA-Z ]/g, '');
-    newStr = newStr.replace(/([A-Z])/g, ' $1').trim();
-    console.log(newStr);
+    return str
+        .split(/\s|_|(?=[A-Z])/) // split str if one of the three condition is encountered: [\s] [_] [?=A-Z]
+        .join('-'
+        .toLowerCase();
 }
 
-/*
 console.log(spinalCase('This is Spinal Tap'));
 console.log(spinalCase('ThisIsSpinalTap'));
 console.log(spinalCase('The_Andy_Griffith_Show'));
 console.log(spinalCase('AllThe-small Things'));
-*/
-//return  str.toLowerCase().replace(/\s/g, '-');
-
-spinalCase('This is Spinal Tap');
-spinalCase('ThisIsSpinalTap');
-spinalCase('The_Andy_Griffith_Show');
-spinalCase('AllThe-small Things');
