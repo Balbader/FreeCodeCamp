f4fe97ffa578646d7ba2e40c9e9c964517005fbf
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-02 22:45:15 +0200

    update

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/search_and_replace.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/search_and_replace.js
@@ -1,5 +1,13 @@
 /*Perform a search and replace on the sentence using the arguments provided and return the new sentence.*/
 
 function myReplace(str, before, after) {
-    return str;
+    str.split();
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === before) {
+            str.replace(after);
+        }
+    }
+    console.log(str);
 }
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
