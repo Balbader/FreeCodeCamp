b2856c204d5e9bf0ba73f4d2e4a0a472476dfa19
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-03 12:09:33 +0200

    update

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/DNA_Pairing.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/DNA_Pairing.js
@@ -7,3 +7,36 @@ For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
 The character and its pair are paired up in an array, and all the arrays are grouped
 into one encapsulating array.
 */
+
+function pairElement(str) {
+    // Return each strand as an array of two elements, the original and the pair.
+    var paired = [];
+
+    // Function to check with strand to pair.
+    var search = function(char) {
+        switch (char) {
+            case "A":
+                paired.push(["A", "T"]);
+                break;
+            case "T":
+                paired.push(["T", "A"]);
+                break;
+            case "C":
+                paired.push(["C", "G"]);
+                break;
+            case "G":
+                paired.push(["G", "C"]);
+                break;
+        }
+    };
+
+    // Loops through the input and pair.
+    for (var i = 0; i < str.length; i++) {
+        search(str[i]);
+    }
+
+    return paired;
+}
+
+
+console.log(pairElement('GCG'));
