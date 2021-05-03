d18a1ff7b4a85c6d57ea549e9bd32b068495aa4c
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-03 12:54:47 +0200

    update;

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/missing_letters.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/missing_letters.js
@@ -4,4 +4,45 @@ If all letters are present in the range, return undefined.
 */
 
 function findLetter(str) {
+    for (let i = 0; i < str.length; i++) {
+        /* code of current character */
+        const code = str.charCodeAt(i);
+
+        /* if code of current character is not equal to first character + no of iteration
+        hence character has been escaped */
+        if (code !== str.charCodeAt(0) + i) {
+            /* if current character has escaped one character find previous char and return */
+            return String.fromCharCode(code - 1);
+        }
+    }
+    return undefined;
+}
+
+console.log(findLetter('abce'));
+
+
+// Variation 1
+function fearNotLetter(str) {
+    var compare = str.charCodeAt(0),
+        missing;
+
+    str.split("").map(function(letter, index) {
+        if (str.charCodeAt(index) == compare) {
+            ++compare;
+        } else {
+            missing = String.fromCharCode(compare);
+        }
+    });
+
+    return missing;
+}
+
+
+// Variation 2
+function fearNotLetter(str) {
+    for (let i = 1; i < str.length; ++i) {
+        if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+            return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+        }
+    }
 }
