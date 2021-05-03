f50382ec88da47d08e2c1ba0157860ecfd148135
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-03 19:16:36 +0200

    update

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/arguments_optional.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/arguments_optional.js
@@ -48,3 +48,34 @@ function addTogether() {
 // test here
 console.log(addTogether(2, 3));
 
+
+// Variation 1
+function addTogether() {
+    const [first, second] = Object.values(arguments);
+    // Check first argument
+    if (typeof first !== "number") {
+        return undefined;
+    }
+
+    // Function to add second argument
+    const addSecond = (second) => typeof second === "number" ? first + second : undefined;
+
+    // Check second argument
+    if (second !== undefined) {
+        return addSecond(second);
+    } else {
+        return addSecond
+    }
+}
+
+
+// Variation 2
+//jshint esversion: 6
+function addTogether() {
+    var args = Array.from(arguments);
+    return args.some(n => typeof n !== "number")
+        ? undefined
+        : args.length > 1
+        ? args.reduce((acc, n) => (acc += n), 0)
+        : n => (typeof n === "number" ? n + args[0] : undefined);
+}
