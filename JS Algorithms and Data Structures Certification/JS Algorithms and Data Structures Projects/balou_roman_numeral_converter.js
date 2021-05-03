e6bb3c07e013889b8f8208e4eaaec0c0dbe56df7
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-03 22:11:53 +0200

    basil version

---
+++ JS Algorithms and Data Structures Certification/JS Algorithms and Data Structures Projects/balou_roman_numeral_converter.js
@@ -0,0 +1,16 @@
+function convertToRoman(num) {
+    const decValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanVal = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let romanNbr = '';
+
+    for (let i = 0; i < decValues.length; i++) {
+        while (decValues[i] <= num) {
+            romanNbr += romanVal[i];
+            num -= decValues[i];
+        }
+    }
+    return romanNbr;
+}
+
+convertToRoman(36);
