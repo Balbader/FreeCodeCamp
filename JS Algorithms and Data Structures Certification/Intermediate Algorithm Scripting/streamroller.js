f827c0b3ad5aa9535f5355ce25495aaf77133ad9
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-03 19:00:39 +0200

    update

---
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/streamroller.js
@@ -0,0 +1,72 @@
+/*Flatten a nested array.
+  You must account for varying levels of nesting.*/
+
+function steamrollArray(arr) {
+    const flattenedArray = [];
+    // Loop over array contents
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            // Recursively flatten entries that are arrays
+            //  and push into the flattenedArray
+            flattenedArray.push(...steamrollArray(arr[i]));
+        } else {
+            // Copy contents that are not arrays
+            flattenedArray.push(arr[i]);
+        }
+    }
+    return flattenedArray;
+
+    // Call the function for each element in the array
+    arr.forEach(flatten);
+    return flattenedArray;
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+
+// Variation 1
+function steamrollArray(arr) {
+    const flat = [].concat(...arr);
+    return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+}
+
+
+// Variation 2
+function steamrollArray(arr) {
+    return arr
+        .toString()
+        .replace(",,", ",") // "1,2,,3" => "1,2,3"
+        .split(",") // ['1','2','3']
+        .map(function(v) {
+            if (v == "[object Object]") {
+                // bring back empty objects
+                return {};
+            } else if (isNaN(v)) {
+                // if not a number (string)
+                return v;
+            } else {
+                return parseInt(v); // if a number in a string, convert it
+            }
+        });
+}
+
+
+// Variation 3
+function steamrollArray(val,flatArr=[]) {
+    val.forEach(item => {
+        if (Array.isArray(item)) steamrollArray(item, flatArr);
+        else flatArr.push(item);
+    });
+    return flatArr;
+}
+
+
+// Variation 4
+function steamrollArray(arr, flatArr = []) {
+    const elem = arr.pop();
+    return elem
+        ? !Array.isArray(elem)
+        ? steamrollArray(arr, [elem, ...flatArr])
+        : steamrollArray(arr.concat(elem), flatArr)
+        : flatArr;
+}
