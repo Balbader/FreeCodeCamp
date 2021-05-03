ca9a7237a36d088033c18754257ca009c7df0428
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-03 12:04:19 +0200

    update

--- JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/search_and_replace.js
+++ JS Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/search_and_replace.js
@@ -121,3 +121,17 @@ function myReplace(str, before, after) {
 
     return str.replace(regex, replacingStr);
 }
+
+
+// Vartiation 4
+function myReplace(str, before, after) {
+    const myArr = str.split(" ");
+    const [wordToReplace] = myArr.filter(item => item === before);
+    return wordToReplace[0].toUpperCase() !== wordToReplace[0]
+        ? myArr.map(item => (item === before ? after : item)).join(" ")
+        : myArr
+        .map(item =>
+            item === before ? after[0].toUpperCase() + after.slice(1) : item
+        )
+        .join(" ");
+}
